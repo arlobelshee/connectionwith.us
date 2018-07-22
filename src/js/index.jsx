7497a7a380dd72ce23fbd5a21bd00ae6f652a05c
Arlo Belshee <arlo.belshee@live.com>
2018-07-21 21:54:34 -0700

    F    Finished the drinks sliders section. The inputs all work. Of course, no save for anything yet. And I need to get more drinks in there. But it's a start!

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,48 +1,28 @@
 "use strict";
 
-class LikeButton extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { liked: false };
-	}
-
-	render() {
-		if (this.state.liked) {
-			return "You liked this.";
-		}
-
-		return <button onClick={() => this.setState({ liked: true })}>Like</button>;
-	}
-}
-
 class ShowMeaning extends React.Component {
 	constructor(props) {
 		props.meanings = [props.no, props.some, props.yes];
 		super(props);
-		this.state = { value: this.props.initialvalue };
+		this.state = { value: props.initialvalue };
+		document.querySelector(
+			"input[name=" + props.fieldname + "]"
+		).oninput = this.onChange.bind(this);
+	}
+
+	onChange(evt) {
+		this.setState({ value: evt.target.value });
 	}
 
 	render() {
 		return (
-			<div>
-				<input
-					type="range"
-					name={this.props.fieldname}
-					min="0"
-					max="2"
-					value={this.state.value}
-					onChange={evt => this.setState({ value: evt.target.value })}
-				/>
-				<span class="meaning">{this.props.meanings[this.state.value]}</span>
-			</div>
+				<span>{this.props.meanings[this.state.value]}</span>
 		);
 	}
 }
 
-let domContainer = document.querySelectorAll(".meanings");
-for (let i = 0; i < domContainer.length; i++) {
-	ReactDOM.render(
-		<ShowMeaning {...domContainer[i].dataset} />,
-		domContainer[i]
+document
+	.querySelectorAll(".meaning")
+	.forEach(domContainer =>
+		ReactDOM.render(<ShowMeaning {...domContainer.dataset} />, domContainer)
 	);
-}
