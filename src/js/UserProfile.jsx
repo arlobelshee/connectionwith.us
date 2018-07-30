a01a8635d8ae1d23e5ad6888dc5a6e128c501a78
Arlo Belshee <arlo.belshee@live.com>
2018-07-29 22:19:01 -0700

    F    Style the modal that appears to ask people for info.

--- src/js/UserProfile.jsx
+++ src/js/UserProfile.jsx
@@ -34,33 +34,46 @@ export class UserProfile extends React.Component {
 	render() {
 		if (this.user_data.needsName) {
 			return (
-				<ReactModal isOpen={this.user_data.needsName} appElement={document.getElementById("main")} >
+				<ReactModal
+					isOpen={this.user_data.needsName}
+					appElement={document.body}
+					className="open-modal"
+					overlayClassName="open-modal-overlay"
+				>
 					<h2>So, I forgot to ask...</h2>
 					<p>Who is this?</p>
 					<p>
 						If this is multiple people, then come back multiple times! We need
 						personal info from each of you!
 					</p>
+					<p>
+						If you need to log out later, just click on your name in the nav
+						(it'll appear as soon as you are done here).
+					</p>
 					<form onSubmit={this.closeDialog}>
-						<input
-							name="name"
-							type="text"
-							placeholder="Your name"
-							// value={this.user_data.name}
-						/>
-						<input
-							name="consent"
-							type="checkbox"
-							id="consent"
-							// checked={this.user_data.accepted_data_tracking}
-						/>
-						<label htmlFor="consent">
-							As per GDPR, I consent to let you gather info about me in order to
-							organize this event, especially to ensure we have the right drinks
-							and to make gratuitous analyses of how you use the site. We will
-							totally track you on this site &mdash; both directly, and via
-							third-party cookies!
-						</label>
+						<p>
+							<input
+								name="name"
+								type="text"
+								placeholder="Your name"
+								// value={this.user_data.name}
+							/>
+						</p>
+						<p>
+							<input
+								name="consent"
+								type="checkbox"
+								id="consent"
+								// checked={this.user_data.accepted_data_tracking}
+							/>
+							<label htmlFor="consent">
+								As per GDPR, I consent to let you gather info about me in order
+								to organize this event, especially to ensure we have the right
+								drinks and to make gratuitous analyses of how you use the site.
+								We will totally track you on this site &mdash; both directly,
+								and via third-party cookies!
+							</label>
+						</p>
 						<p>
 							You don't have to accept the tracking. But if you don't, then I
 							can't save any of your data, so we'll never know what to invite
@@ -73,7 +86,7 @@ export class UserProfile extends React.Component {
 		}
 		if (this.user_data.name) {
 			return (
-				<a onClick={this.logOut}>
+				<a onClick={this.logOut} title="log out and become someone else">
 					<i className="fas fa-user-astronaut">&nbsp;</i> hi,{" "}
 					{this.user_data.name}
 				</a>
